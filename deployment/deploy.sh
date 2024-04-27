15d7548b40228c9c0d6a73a8d32994577774bec3
gospodin66 <borna986@gmail.com>
2024-04-28 01:39:59 +0200

    1.2.1

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -2,15 +2,14 @@
 cargo build --bin rust-tcp-http
 cargo build --bin client
 
-docker build -t rust-server:1.0 .. -f DockerfileServer 
+docker build -t rust-server:1.0 .. -f DockerfileServer && docker tag rust-server:1.0 127.0.0.1:5000/v2/rust-server:1.0
+docker build -t rust-nginx:1.0 .. -f DockerfileNginx && docker tag rust-nginx:1.0 127.0.0.1:5000/v2/rust-nginx:1.0
 docker build -t rust-client:1.0 .. -f DockerfileClient
 docker build -t rust-mysql:1.0 .. -f DockerfileMysql 
-docker build -t rust-nginx:1.0 .. -f DockerfileNginx
 
-docker tag rust-server:1.0 127.0.0.1:5000/v2/rust-server:1.0
+
 docker tag rust-mysql:1.0 127.0.0.1:5000/v2/rust-mysql:1.0
 docker tag rust-client:1.0 127.0.0.1:5000/v2/rust-client:1.0
-docker tag rust-nginx:1.0 127.0.0.1:5000/v2/rust-nginx:1.0
 
 
 docker push 127.0.0.1:5000/v2/rust-server:1.0
@@ -29,13 +28,15 @@ docker push 127.0.0.1:5000/v2/rust-nginx:1.0
 #sudo k3s ctr images import rust-mysql.tar
 
 
-# rm image dir
-sudo rm -rf /var/lib/docker/volumes/registry-storage/_data/docker/registry/v2/repositories/v2/rust-server
-
-# run garbage collect
+# rm image dir & run garbage collect
+sudo rm -rf /var/lib/docker/volumes/registry-storage/_data/docker/registry/v2/repositories/v2/rust-server && \
+sudo rm -rf /var/lib/docker/volumes/registry-storage/_data/docker/registry/v2/repositories/v2/rust-nginx && \
 docker exec -it private-registry bin/registry garbage-collect /etc/docker/registry/config.yml
 
+# uninstall
+helm uninstall server client base mysql nginx
 
+# install
 helm upgrade --install server helm-chart-server
 helm upgrade --install client helm-chart-client
 helm upgrade --install base helm-chart-base
